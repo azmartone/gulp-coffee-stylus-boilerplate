03907752e68b6cf6b5e2891e57fd39b61b9c577c
Danny Duong <dannyduong888@gmail.com>
2015-02-18 00:38:00 -0800

    Add del, copy, inject

--- gulpfile.js
+++ gulpfile.js
@@ -4,73 +4,61 @@ var inject = require("gulp-inject");
 var rename = require('gulp-rename');
 var stylus = require('gulp-stylus');
 var gutil = require('gulp-util');
+var del = require('del');
 var nib = require('nib');
 
 
-// gulp.task('scripts', function() {
-// 	gulp.src('./src/scripts/*.coffee')
-// 		.pipe(coffee({bare: true}).on('error', gutil.log))
-// 		.pipe(gulp.dest('./build/scripts/'))
-// });
-
 gulp.task('scripts', function() {
-  gulp.src('src/scripts/main.coffee', { read: false })
-    .pipe(browserify({
-      transform: ['coffeeify'],
-      extensions: ['.coffee']
-    }))
-    .pipe(rename('main.js'))
-    .pipe(gulp.dest('./build/scripts/'))
+	del(['./build/scripts/**/*.*']);
+	
+	return gulp.src('src/scripts/main.coffee', { read: false })
+		.pipe(browserify({
+			transform: ['coffeeify'],
+			extensions: ['.coffee']
+		}))
+		.pipe(rename('main.js'))
+		.pipe(gulp.dest('./build/scripts/'))
 });
 
-// Options 
-// Options compress 
-gulp.task('styles', function () {
-  gulp.src('./src/styles/*.styl')
-    .pipe(stylus({
-      use: nib(),
-      compress: true
-    }))
-    .pipe(gulp.dest('./build/styles'));
-});
-
-
-// gulp.task('copy', function () {
-//   gulp.src('./src/root/**/*.*')
-//     .pipe(gulp.dest('./build/'));
-
-//   var target = gulp.src('./build/index.html');
-//   // It's not necessary to read the files (will speed up things), we're only after their paths: 
-//   var sources = gulp.src(['./build/scripts/**/*.js', './build/styles/**/*.css'], {read: false});
  
-//   return target.pipe(inject(sources))
-//     .pipe(gulp.dest('./build'));
-// });
+gulp.task('styles', function () {
+	del(['./build/styles/**/*.*']);
+
+	return gulp.src('./src/styles/*.styl')
+		.pipe(stylus({
+			use: nib(),
+			compress: true
+		}))
+		.pipe(gulp.dest('./build/styles/'));
+});
+
 
 gulp.task('copy', function () {
-  gulp.src('./src/root/**/*.*')
-    .pipe(gulp.dest('./build/'));
-});
+	del(['./build/*.*']);
 
-gulp.task('index', function () {
-  var target = gulp.src('./build/index.html');
-  // It's not necessary to read the files (will speed up things), we're only after their paths: 
-  var sources = gulp.src(['./build/scripts/**/*.js', './build/styles/**/*.css'], {read: false});
- 
-  return target.pipe(inject(sources))
-    .pipe(gulp.dest('./build'));
+	return gulp.src('./src/root/**/*.*')
+		.pipe(gulp.dest('./build/'));
 });
 
 
-gulp.task('default', ['scripts', 'styles', 'copy', 'index'], function() {
-    // //a list of watchers, so it will watch all of the following files waiting for changes
-    // gulp.watch('app/scripts/src/**', ['scripts']);
-    // gulp.watch('app/styles/scss/**', ['styles']);
-    // gulp.watch('app/images/**', ['images']);
-    // gulp.watch('app/*.html', ['html']);
+gulp.task('watch', function() {
+    gulp.watch('./src/scripts/**/*.coffee', ['scripts']);
+    gulp.watch('./src/styles/**/*.stylus', ['styles']);
 });
 
-//this is our deployment task, it will set everything for deployment-ready files
-// gulp.task('deploy', ['clean'], function () {
-//   gulp.start('scripts-deploy', 'styles-deploy', 'html-deploy', 'images-deploy');
-// });
+//ToDo:
+//ImageMin
+//Copy Media
+//JS Concat
+//JS Minify
+//Bower
+
+gulp.task('default', ['scripts', 'styles', 'copy', 'watch'], function () {
+	var target = gulp.src('./build/index.html');
+
+	var sources = gulp.src(['./build/scripts/**/*.js', './build/styles/**/*.css'],
+		{read: false});
+
+	return target.pipe(inject(sources, {addRootSlash:false, ignorePath:'build'}))
+		.pipe(gulp.dest('./build'));
+});
