10c3f4b444a3b6bb60dbaa5d0b06eadb502810f6
Danny Duong <danny.duong@trailerpark.com>
2015-02-19 11:43:44 -0800

    Add config paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var browserSync = require('browser-sync');
 var debowerify = require('debowerify');
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
@@ -5,86 +6,138 @@ var inject = require("gulp-inject");
 var rename = require('gulp-rename');
 var stylus = require('gulp-stylus');
 var gutil = require('gulp-util');
-var del = require('del');
-var nib = require('nib');
-
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
 
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
+var del = require('del');
+var nib = require('nib');
+
+
+var reload = browserSync.reload;
+
+var Config = {
+	paths: {
+		src:   {
+			root:     'src/root',
+			styles:   'src/styles',
+			scripts:  'src/scripts',
+			media:    'src/media'
+		},
+		build: {
+			root:     'build',
+			styles:   'build/styles',
+			scripts:  'build/scripts',
+			media:    'build/media'
+		},
+		dist: {
+			root:     'dist',
+			styles:   'dist/styles',
+			scripts:  'dist/scripts',
+			media:    'dist/media'
+		}
+	}
+}
+
+/***** SCRIPTS *****/
 
 gulp.task('scripts', function() {
-	del(['./build/scripts/**/*.*']);
+	del([ Config.paths.build.scripts + '/**/*.*' ]);
 	
-	return gulp.src('src/scripts/main.coffee', { read: false })
+	return gulp.src( Config.paths.src.scripts + '/main.coffee', { read: false })
 		.pipe(browserify({
 			transform: ['coffeeify', 'debowerify'],
 			extensions: ['.coffee', '.js']
 		}))
 		.pipe(rename('main.js'))
-		.pipe(gulp.dest('./build/scripts/'))
+		.pipe(gulp.dest( Config.paths.build.scripts + '/' ))
         .pipe(browserSync.reload({stream:true}));
 });
 
  
-gulp.task('styles', function () {
-	del(['./build/styles/**/*.*']);
+/***** STYLES *****/
 
-	return gulp.src('./src/styles/**/*.styl')
+gulp.task('styles', function () {
+	del([ Config.paths.build.styles + '/**/*.*' ]);
+
+	return gulp.src( Config.paths.src.styles + '/**/*.styl' )
 		.pipe(stylus({
 			use: nib(),
 			compress: true
 		}))
-		.pipe(gulp.dest('./build/styles/'))
-        .pipe(browserSync.reload({stream:true}));
+		.pipe(gulp.dest( Config.paths.build.styles + '/' ))
+		.pipe(browserSync.reload({stream:true}));
+});
+
+/***** MEDIA *****/
+
+gulp.task('media', function () {
+	gulp.src( Config.paths.src.media + '/**/*.*' )
+		.pipe(gulp.dest( Config.paths.build.media ));
+});
+
+gulp.task('media-deploy', function () {
+    return gulp.src( Config.paths.src.media + '/**/*.*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest( Config.paths.build.media ));
 });
 
 
-gulp.task('copy', function () {
-	del(['./build/*.*']);
+/**********/
 
-	gulp.src('./src/root/**/*.*')
-		.pipe(gulp.dest('./build/'));
-
-	gulp.src('./src/media/**/*.*')
-		.pipe(gulp.dest('./build/media'));
+gulp.task('clean', function () {
+	del([ Config.paths.build.root + '/*.*' ]);
 });
 
+
+gulp.task('index', function () {
+	gulp.src( Config.paths.src.root + '/**/*.*' )
+		.pipe(gulp.dest( Config.paths.build.root + '/' ));
+});
 
 gulp.task('watch', function() {
-    gulp.watch('./src/scripts/**/*.coffee', ['scripts']);
-    gulp.watch('./src/styles/**/*.styl', ['styles']);
+    gulp.watch( Config.paths.src.scripts + '/**/*.coffee', ['scripts']);
+    gulp.watch( Config.paths.src.styles + '/**/*.styl', ['styles']);
     gulp.watch('./gulpfile.js', ['default']);
 });
 
 gulp.task('browser-sync', ['default'], function() {
     browserSync({
         server: {
-            baseDir: "./build/"
+            baseDir:  Config.paths.build.root
         }
     });
 });
+
+
+/***** MAIN TASKS *****/
 
 gulp.task('serve', ['browser-sync'], function () {
 
 });
 
-gulp.task('default', ['scripts', 'styles', 'copy', 'watch'], function () {
-	var target = gulp.src('./build/index.html');
+gulp.task('default', ['scripts', 'styles', 'index', 'media', 'watch'], function () {
+	var target = gulp.src( Config.paths.build.root + '/index.html');
 
-	var sources = gulp.src(['./build/scripts/**/*.js', './build/styles/**/*.css'],
+	var sources = gulp.src([ 
+			Config.paths.build.scripts + '/**/*.js',
+			Config.paths.build.styles + '/**/*.css'],
 		{read: false});
 
 	return target.pipe(inject(sources, {addRootSlash:false, ignorePath:'build'}))
-		.pipe(gulp.dest('./build'));
+		.pipe(gulp.dest( Config.paths.build.root ));
 });
 
 
-//ToDo:
-//Deploy Stuff
-//Config folder for deploy
-//ImageMin
-//JS Concat
-//JS Minify
+gulp.task('deploy', ['clean', 'scripts', 'styles', 'index', 'media-deploy', 'watch'], function () {
+	//ToDo:
+	//Deploy Stuff
+	//Config folder for deploy
+	//ImageMin
+	//JS Concat
+	//JS Minify
+});
